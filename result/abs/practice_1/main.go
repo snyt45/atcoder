6b9f3d2d9174d484f8ade5e54302a1a4a1fa0e62
yuta.sano <snyt45@gmail.com>
2025-01-05 14:07:22 +0000

    abc practice_1

---
+++ result/abs/practice_1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	b, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	c, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	s := sc.Text()
+	fmt.Printf("%d %s\n", a+b+c, s)
+}
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, err := os.ReadFile("./input")
+		if err != nil {
+			panic(err)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
